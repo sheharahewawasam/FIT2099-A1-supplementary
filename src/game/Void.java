225cfcd3f1bb9eb403d529f10ca968e9c2c88463
sheharahewawasam <126374930+sheharahewawasam@users.noreply.github.com>
2024-01-10 21:13:11 +0530

    adding voids to the ground
    at the instance player step into the void the fancy message died is displayed and the player is made unconscious in that location.

---
+++ src/game/Void.java
@@ -0,0 +1,31 @@
+
+package game;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.displays.Display;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+
+public class Void extends Ground {
+    public Void() {
+        super('+');
+    }
+
+    @Override
+    public void tick(Location location) {
+        if (location.containsAnActor()) {
+            Actor actor = location.getActor();
+            if (actor instanceof Player) {
+                Display display = new Display();
+                display.println("\n");
+                for (String line : FancyMessage.YOU_DIED.split("\n")) {
+                    display.println(line);
+                }
+                display.println("\n");
+            }
+            // Make the actor unconscious after displaying the message
+            actor.unconscious(location.map());
+        }
+    }
+}
+
