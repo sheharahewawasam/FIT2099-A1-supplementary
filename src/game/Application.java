954e77d451fe3bee26017a67be8f8385fbe2d0a5
sheharahewawasam <126374930+sheharahewawasam@users.noreply.github.com>
2024-01-12 08:51:14 +0530

    new items in the maps are added

--- src/game/Application.java
+++ src/game/Application.java
@@ -69,6 +69,11 @@ public class Application {
         // Add the Burial Ground map to the world
         world.addGameMap(burialGroundMap);
 
+        LockedGate lockedGate1 = new LockedGate();//Gate created for abandonedVillageMap to burialGroundMap
+        LockedGateBG lockedGate2 = new LockedGateBG();//Gate created for burialGroundMap to abandonedVillageMap
+        abandonedVillageMap.at(29, 0).setGround(lockedGate1);//the x and y labels set for the gate to be implemented in map of abandonedVillageMap
+        burialGroundMap.at(19, 0).setGround(lockedGate2);///the x and y labels set for the gate to be implemented in map of burialGroundMap
+
         for (String line : FancyMessage.TITLE.split("\n")) {
             new Display().println(line);
             try {
@@ -85,10 +90,17 @@ public class Application {
 
         abandonedVillageMap.at(24,5).addItem(new Broadsword());
 
-        graveyard graveyard1 = new graveyard();
-        graveyard graveyard2 = new graveyard();
+        graveyard graveyard1 = new graveyard(abandonedVillageMap, burialGroundMap);
+        graveyard graveyard2 = new graveyard(abandonedVillageMap, burialGroundMap);
         abandonedVillageMap.at(35, 0).setGround(graveyard1);
         abandonedVillageMap.at(31, 11).setGround(graveyard2);
+
+        graveyard graveyard3 = new graveyard(abandonedVillageMap, burialGroundMap);
+        graveyard graveyard4 = new graveyard(abandonedVillageMap, burialGroundMap);
+        burialGroundMap.at(27, 5).setGround(graveyard3);
+        burialGroundMap.at(26, 11).setGround(graveyard4);
+
+
 
         world.run();
     }
