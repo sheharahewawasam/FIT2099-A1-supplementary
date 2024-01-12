cdab307ceebc6f13932e41d752532547efedf38d
sheharahewawasam <126374930+sheharahewawasam@users.noreply.github.com>
2024-01-12 08:29:50 +0530

    The gate in burialground and its opening fucntion
    When the actor gets closed to the gate, it is checked whether the gate is already unlocked or the actor has the key to unlock the gate, if so the actor is allowed to step into the gate, and after the map changes

---
+++ src/game/LockedGateBG.java
@@ -0,0 +1,90 @@
+package game;
+
+import java.util.List;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actions.ActionList;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.Ground;
+import edu.monash.fit2099.engine.positions.Location;
+import game.Status;
+import game.OpenLockedGateBGAction;
+import game.Player;
+
+/**
+ * A class that represents a LockedGate ground.
+ * LockedGate is a type of Ground that can only be entered if it has the UNLOCKED capability.
+ * It provides actions to unlock the gate and transition to another map.
+ * Created by:
+ * Riordan D. Alfredo
+ * Modified by:
+ *
+ * @see Ground
+ */
+public class LockedGateBG extends Ground {//New Locked Gate to Burial Ground Map created from Ancient Woods
+
+    private Action mapTransitionAction;
+
+    /**
+     * Constructor for the LockedGate class.
+     * Initializes a LockedGate object with the display character '='.
+     */
+    public LockedGateBG() {
+        super('=');
+    }// Symbol given for Gate which is =
+
+    /**
+     * Adds a map transition action for the LockedGate.
+     *
+     * @param newAction The Action to set as the map transition action.
+     */
+    public void addMapTransitionAction(Action newAction) {
+        this.mapTransitionAction = newAction;
+    }
+
+    /**
+     * Determines whether an actor can enter this LockedGate ground.
+     *
+     * @param actor The actor that wants to enter.
+     * @return True if the gate has the UNLOCKED capability, allowing the actor to enter; otherwise, false.
+     */
+    @Override
+    public boolean canActorEnter(Actor actor) {
+        return this.hasCapability(Status.UNLOCKED);
+    }
+
+    /**
+     * Provides a list of allowable actions for an actor interacting with this LockedGate.
+     *
+     * @param actor     The actor interacting with the LockedGate.
+     * @param location  The Location where this Ground is located.
+     * @param direction String representing the direction of interaction.
+     * @return An ActionList containing allowable actions based on actor's capabilities and gate's status.
+     */
+    @Override
+    public ActionList allowableActions(Actor actor, Location location, String direction) {
+        ActionList actions = new ActionList();
+        List<Exit> adjacentLocations = location.getExits();
+
+        // Check each adjacent location for the presence of a player
+        for (Exit exit : adjacentLocations) {
+            if (exit.getDestination().containsAnActor()) {
+                Actor actor1 = exit.getDestination().getActor();
+
+                // Check if the actor is an instance of Player
+                if (actor1 instanceof Player) {
+                    //// Add the capability to indicate that the player is nearby the gate and whether the player can enter the gate or not.It also checks whether the key is present or not.
+                    if (actor.hasCapability(Status.HAS_KEY) && !this.hasCapability(Status.UNLOCKED)) {
+                        actions.add(new OpenLockedGateBGAction());
+
+                    } else if (this.hasCapability(Status.UNLOCKED)) {
+                        actions.add(mapTransitionAction);
+                        break;
+                    }
+                }
+            }
+        }
+        return actions;
+    }
+}
---
+++ src/game/OpenLockedGateBGAction.java
@@ -0,0 +1,55 @@
+package game;
+
+import edu.monash.fit2099.engine.actions.Action;
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.positions.Exit;
+import edu.monash.fit2099.engine.positions.GameMap;
+import edu.monash.fit2099.engine.positions.Location;
+import game.LockedGate;
+import game.Status;
+
+import java.util.List;
+
+/**
+ * An Action class representing the action of an Actor opening a locked gate on a GameMap.
+ * This action unlocks the gate by adding the UNLOCKED capability to the LockedGate ground.
+ */
+public class OpenLockedGateBGAction extends Action {
+
+    /**
+     * Executes the action of opening a locked gate.
+     *
+     * @param actor The Actor performing the action.
+     * @param map   The GameMap in which the action is performed.
+     * @return A message indicating the result of the action.
+     */
+    @Override
+    public String execute(Actor actor, GameMap map) {
+        // Get the list of exits from the current location of the actor
+        List<Exit> exits = map.locationOf(actor).getExits();
+
+        // Find the exit that leads to the LockedGate ground and unlock it
+        for (Exit exit : exits) {
+            Location adjacentLocation = exit.getDestination();
+            if (adjacentLocation.getGround() instanceof LockedGateBG) {
+                // Add the UNLOCKED capability to the LockedGate ground
+                adjacentLocation.getGround().addCapability(Status.UNLOCKED);
+                break; // Assuming there is only one LockedGate to unlock
+            }
+        }
+
+        // Return a message indicating that the locked gate is unlocked
+        return "The locked gate is unlocked";
+    }
+
+    /**
+     * Provides a description of the action for display in a menu.
+     *
+     * @param actor The Actor performing the action.
+     * @return A menu description of the action.
+     */
+    @Override
+    public String menuDescription(Actor actor) {
+        return actor + " opens the locked gate";
+    }
+}
