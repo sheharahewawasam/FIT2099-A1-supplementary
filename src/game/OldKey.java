bfb096d20dc8d757ac1be41f542c5a6b791447b4
sheharahewawasam <126374930+sheharahewawasam@users.noreply.github.com>
2024-01-12 08:20:27 +0530

    OldKey implementation
    with the pickupaction the actor gets the capabillity of HAS_KEY and the item is added to players inventory

---
+++ src/game/OldKey.java
@@ -0,0 +1,50 @@
+package game;
+
+import edu.monash.fit2099.engine.actors.Actor;
+import edu.monash.fit2099.engine.items.DropAction;
+import edu.monash.fit2099.engine.items.Item;
+import edu.monash.fit2099.engine.items.PickUpAction;
+import game.Status;
+
+/**
+ * A class that represents an Old Key item.
+ * Old Keys can be picked up by actors and are used to unlock certain locked gates.
+ * Created by:
+ * Riordan D. Alfredo
+ * Modified by:
+ *
+ * @see Item
+ */
+public class OldKey extends Item {
+
+    /**
+     * Constructor for the OldKey class.
+     * Initializes an Old Key object with the name "Old Key", display character '-', and stackable as true.
+     */
+    public OldKey() {
+        super("Old Key", '-', true);
+    }
+
+    /**
+     * Retrieves the PickUpAction for this Old Key when an actor tries to pick it up.
+     *
+     * @param actor The actor attempting to pick up the Old Key.
+     * @return A PickUpAction if the actor is allowed to pick up the Old Key, null otherwise.
+     */
+    @Override
+    public PickUpAction getPickUpAction(Actor actor) {
+        actor.addCapability(Status.HAS_KEY);
+        return super.getPickUpAction(actor);
+    }
+
+    /**
+     * Retrieves the DropAction for this Old Key when an actor tries to drop it.
+     *
+     * @param actor The actor attempting to drop the Old Key.
+     * @return Always returns null because Old Keys cannot be dropped once picked up.
+     */
+    @Override
+    public DropAction getDropAction(Actor actor) {
+        return null; // Old Keys cannot be dropped once picked up.
+    }
+}
