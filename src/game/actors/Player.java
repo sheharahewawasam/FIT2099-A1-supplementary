da6e3e36ef165cfc08ad0b5bd5c0275587f41099
sheharahewawasam <126374930+sheharahewawasam@users.noreply.github.com>
2024-01-15 14:00:46 +0530

    correction
    stamina 200

--- src/game/actors/Player.java
+++ src/game/actors/Player.java
@@ -34,7 +34,7 @@ public class Player extends Actor {
         super(name, displayChar, hitPoints);
         this.addCapability(Status.HOSTILE_TO_ENEMY);
         this.addCapability(Ability.CAN_ENTER_FLOOR);
-        this.addAttribute(STAMINA, new BaseActorAttribute(100));
+        this.addAttribute(STAMINA, new BaseActorAttribute(200));
     }
 
     @Override
